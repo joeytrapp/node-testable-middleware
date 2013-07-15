48995c835e9489df461440b415ce2c7085607c30
Joey Trapp <joey@loadsys.com>
2013-07-14 19:53:21 -0500

    Updated with docs and correct dependencies

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var testable = require('testable');
+var testable = require('testable'),
+    connect = require('connect');
 
 exports.attach = function(app, config) {
   var route = config.route || '/test.html';
 
   if (route[0] !== '/') { route = '/' + route; }
 
+  app.use(connect.static(testable.assetsPath()));
   app.use(function testableMiddleware(req, res, next) {
     if (new RegExp('^' + route).test(req.url)) {
       res.setHeader('Content-Type', 'text/html');
@@ -13,9 +15,5 @@ exports.attach = function(app, config) {
       next();
     }
   });
-};
-
-exports.assetsPath = function() {
-  return testable.assetsPath();
 };
 
